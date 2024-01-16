a87dae5308d63d3c5519a295304b90bfe8d4e3bf
Agoziem <chiagoziendukwe90@gmail.com>
2024-01-15 22:21:22 -0800

    changes

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,41 +1,19 @@
-let counts1 = setInterval(updated1, 90);
-let counts2 = setInterval(updated2, 90);
-let counts3 = setInterval(updated3, 90);
-let upto1 = 0;
-let upto2 = 0;
-let upto3 = 0;
+function createCounterUpdater(counterId) {
+  let upto = 0;
+  const counter = document.querySelector(counterId);
+  const number = parseInt(counter.dataset.count);
 
-function updated1() {
-    var firstcount = document.querySelector("#counter1");
-    var number1 = parseInt(firstcount.dataset.count);
-    firstcount.innerHTML = ++upto1;
-    if (upto1 === number1) {
-        clearInterval(counts1);
-    }
+  return function() {
+      counter.innerHTML = ++upto;
+      if (upto === number) {
+          clearInterval(counts);
+      }
+  };
 }
 
-function updated2() {
-    const secondcount = document.querySelector("#counter2");
-    try {
-      var number2 = parseInt(secondcount.dataset.count);
-    } catch (error) {
-      console.log(error)
-      var number2 = 0;
-    }
-    secondcount.innerHTML = ++upto2;
-    if (upto2 === number2) {
-        clearInterval(counts2);
-    }
-}
-
-function updated3() {
-    var thirdcount = document.querySelector("#counter3");
-    var number3 = parseInt(thirdcount.dataset.count);
-    thirdcount.innerHTML = ++upto3;
-    if (upto3 === number3) {
-        clearInterval(counts3);
-    }
-}
+let counts1 = setInterval(createCounterUpdater("#counter1"), 90);
+let counts2 = setInterval(createCounterUpdater("#counter2"), 90);
+let counts3 = setInterval(createCounterUpdater("#counter3"), 90);
 
 // contact form Logic
 
